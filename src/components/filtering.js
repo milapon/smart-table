021eeb078670b399c75e6e5e5b68f7a2c51551c5
Liudmila Ponomarenko <laponomarenko86@gmail.com>
2025-09-21 14:30:04 +0300

    проверка автотестов

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,40 +1,48 @@
 export function initFiltering(elements) {
+    // @todo: #4.1 — заполнить выпадающие списки опциями
     const updateIndexes = (elements, indexes) => {
-        Object.keys(indexes).forEach((elementName) => {
-            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
-                const el = document.createElement('option');
-                el.textContent = name;
-                el.value = name;
-                return el;
-            }))
-        })
-    }
-
+      Object.keys(indexes).forEach((elementName) => {
+        elements[elementName].append(
+          ...Object.values(indexes[elementName]).map((name) => {
+            const option = document.createElement("option");
+            option.textContent = name;
+            option.value = name;
+            return option;
+          })
+        );
+      });
+    };
+  
     const applyFiltering = (query, state, action) => {
-        // код с обработкой очистки поля
-        if (action && action.name === 'clear') {
-            const input = action.closest('label').querySelector('input');
-            input.value = '';
-            state[action.dataset.field] = '';
+      // @todo: #4.2 — обработать очистку поля
+      if (action && action.name === "clear") {
+        const field = action.dataset.field;
+        const input = action.closest(".filter-wrapper").querySelector("input");
+        input.value = "";
+        state[field] = "";
+      }
+  
+      // @todo: #4.5 — отфильтровать данные используя компаратор
+      const filter = {};
+      Object.keys(elements).forEach((key) => {
+        if (elements[key]) {
+          if (
+            ["INPUT", "SELECT"].includes(elements[key].tagName) &&
+            elements[key].value
+          ) {
+            // ищем поля ввода в фильтре с непустыми данными
+            filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+          }
         }
-
-        // @todo: #4.5 — отфильтровать данные, используя компаратор
-        const filter = {};
-        Object.keys(elements).forEach(key => {
-            if (elements[key]) {
-                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
-                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
-                }
-            }
-        })
-
-        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
-    }
-
+      });
+  
+      return Object.keys(filter).length
+        ? Object.assign({}, query, filter)
+        : query; // если в фильтре что-то добавилось, применим к запросу
+    };
+  
     return {
-        updateIndexes,
-        applyFiltering
-    }
-}
-
-
+      updateIndexes,
+      applyFiltering,
+    };
+  }
--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,46 +1,67 @@
-import {getPages} from "../lib/utils.js";
+import { getPages } from "../lib/utils.js";
 
-export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
-    const pageTemplate = pages.firstElementChild.cloneNode(true);
-    pages.firstElementChild.remove();
+let pageCount;
 
-    let pageCount;
+export const initPagination = (
+  { pages, fromRow, toRow, totalRows },
+  createPage
+) => {
+  // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+  const pageTemplate = pages.firstElementChild.cloneNode(true);
+  pages.firstElementChild.remove();
 
-    const applyPagination = (query, state, action) => {
-        const limit = state.rowsPerPage;
-        let page = state.page;
+  const applyPagination = (query, state, action) => {
+    const limit = state.rowsPerPage;
+    let page = state.page;
+    // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
 
-    // переносим код, который делали под @todo: #2.6
-        if (action) switch(action.name) {
-                case 'prev': page = Math.max(1, page - 1); break;
-                case 'next': page = Math.min(pageCount, page + 1); break;
-                case 'first': page = 1; break;
-                case 'last': page = pageCount; break;
-            };
+    // @todo: #2.6 — обработать действия
 
-        return Object.assign({}, query, { // добавим параметры к query, но не изменяем исходный объект
-            limit,
-            page
-        });
-    }
+    if (action)
+      switch (action.name) {
+        case "prev":
+          page = Math.max(1, page - 1);
+          break;
+        case "next":
+          page = Math.min(pageCount, page + 1);
+          break;
+        case "first":
+          page = 1;
+          break;
+        case "last":
+          page = pageCount;
+          break;
+      }
 
-    const updatePagination = (total, { page, limit }) => {
+    return Object.assign({}, query, {
+      // добавим параметры к query, но не изменяем исходный объект
+      limit,
+      page,
+    });
+  };
+
+  const updatePagination = (total, { page, limit }) => {
     pageCount = Math.ceil(total / limit);
+    // @todo: #2.4 — получить список видимых страниц и вывести их
 
-    // переносим код, который делали под @todo: #2.4
     const visiblePages = getPages(page, pageCount, 5);
-        pages.replaceChildren(...visiblePages.map(pageNumber => {
-            const el = pageTemplate.cloneNode(true);
-            return createPage(el, pageNumber, pageNumber === page);
-        }));
-    // переносим код, который делали под @todo: #2.5 (обратите внимание, что rowsPerPage заменена на limit)
-        fromRow.textContent = (page - 1) * limit + 1;
-        toRow.textContent = Math.min((page * limit), total); // data.length на total
-        totalRows.textContent = total; // data.length на total
-    }
+    pages.replaceChildren(
+      ...visiblePages.map((pageNumber) => {
+        const el = pageTemplate.cloneNode(true);
+        return createPage(el, pageNumber, pageNumber === page);
+      })
+    );
 
-        return {
-        updatePagination,
-        applyPagination
-    }; 
-}
+    // @todo: #2.5 — обновить статус пагинации
+
+    fromRow.textContent = (page - 1) * limit + 1;
+    toRow.textContent = Math.min(page * limit, total);
+    totalRows.textContent = total;
+
+    // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
+  };
+  return {
+    updatePagination,
+    applyPagination,
+  };
+};
